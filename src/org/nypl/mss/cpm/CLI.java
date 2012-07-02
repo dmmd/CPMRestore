375e86fe228d0eb0c2fefb5e3b059ab10b2e1ad5
Donald Mennerich <donaldmennerich@nypl.org>
2012-07-02 11:50:39 -0400

    CLI first steps

--- src/org/nypl/mss/cpm/CLI.java
+++ src/org/nypl/mss/cpm/CLI.java
@@ -1,14 +1,43 @@
 package org.nypl.mss.cpm;
 
+import org.apache.commons.cli.*;
+
 public class CLI {
-    public CLI(){
+    
+    private String[] arguments;
+    private Options options;
+    
+    public CLI(String[] args) throws ParseException{
+        this.arguments = args;
+        options = createOptions();
+        testOptions();
         
     }
+
+    private static Options createOptions() {
+ 
+        Options mOptions = new Options();
+
+        OptionBuilder.withArgName("String");
+        OptionBuilder.hasArg(true);
+        OptionBuilder.withDescription("cpm disk definition for image");
+        OptionBuilder.isRequired(true);
+        mOptions.addOption(OptionBuilder.create("f"));
+
+
+        mOptions.addOption(new Option("log", "log activities to the console"));
+
+        return mOptions;
+    }
+
+    private void testOptions() throws ParseException {
+        CommandLineParser parser = new PosixParser();
+        CommandLine cmd = parser.parse( options, arguments);
+    }
     
-    public static void main(String[] args){
-        for(String s: args){
-            System.out.println(s);
-        }
+        
+    public static void main(String[] args) throws ParseException{
+        CLI cli = new CLI(args);
     }
     
 }
